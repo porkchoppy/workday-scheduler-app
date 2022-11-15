c2041468df8e3c05483355f86b2679cf4885dee1
Christin Carter <raspberry.muppet@gmail.com>
2022-11-14 21:51:18 -0700

    finished readme

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,41 +1,36 @@
-
 $(function () {
-
   const timeBox = $(".time-block");
   const currentTime = dayjs().format("H");
 
   //saving to local storage
-  $('.saveBtn').on('click', function () {
+  $(".saveBtn").on("click", function () {
     localStorage.setItem($(this).parent().attr("id"), $(this).prev().val());
   });
 
   //retrieving local storage
   for (let index = 0; index < localStorage.length; index++) {
-      let storageKey = localStorage.key(index);
-        $("#" + storageKey).children("textarea").val(localStorage.getItem(storageKey));   
-      
-
+    let storageKey = localStorage.key(index);
+    $("#" + storageKey)
+      .children("textarea")
+      .val(localStorage.getItem(storageKey));
   }
 
   //adding classes of past, present, or future
   for (let index = 0; index < timeBox.length; index++) {
-    let hourIndex= timeBox[index].id;
+    let hourIndex = timeBox[index].id;
     let currentHour = hourIndex[5] + hourIndex[6];
     if (currentTime == currentHour) {
-      $("#" + hourIndex).addClass("present")
+      $("#" + hourIndex).addClass("present");
     } else if (currentTime > currentHour) {
-      $("#" + hourIndex).addClass("past") 
+      $("#" + hourIndex).addClass("past");
     } else {
-      $("#" + hourIndex).addClass("future")
+      $("#" + hourIndex).addClass("future");
     }
-    }
-
-
+  }
 
   // displaying current date and time in header
   let now = dayjs().format("dddd, MMMM D, YYYY");
   let x = document.getElementById("header");
 
   $("#currentDay").text(now);
-
 });
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# workday-scheduler-app
+# Workday Scheduler App 
+
+## Description
+This repository links to a scheduler in which each hour of your workday has a text box to record tasks, which are then saved to local storage. The text boxes are color coded based on whether the hour is in the past, present, or future. The data entered into the text boxes is saved in local storage to retain said data when the page is refreshed. 
+
+
+## Usage
+
+Here is an example of the scheduler with data input and boxes color coded by past, present, and future time. 
+
+![scheduler screenshot](assets/images/scheduler-1.png)
+
+Along with a screenshot of the local storage reflecting the input data. 
+
+![local-storage screenshot](assets/images/local-storage-1.png)
+
+## Credits
+
+I received collaborative brainstorming and help from my tutor, Andrew Tirpok, as well as classmates Artie Cann and Joshua Taylor. 
+
+## Link to live site
+
+https://porkchoppy.github.io/workday-scheduler-app
